abc99633d8ddd7d3575fe8c715ea4c2927fecf80
Jan Martijn van der Werf <janmartijn@vdwerf.eu>
2020-01-09 10:58:51 +0100

    Empty Maven Archetype for CPNProcessor

---
+++ cpnprocessor/src/main/java/org/informationsystem/ismsuite/cpnprocessor/App.java
@@ -0,0 +1,13 @@
+package org.informationsystem.ismsuite.cpnprocessor;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
