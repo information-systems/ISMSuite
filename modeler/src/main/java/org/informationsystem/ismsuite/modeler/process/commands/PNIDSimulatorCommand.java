37c7b266ef700c8f839618fc8a73f7a7840c92af
Jan Martijn van der Werf <janmartijn@architecturemining.org>
2020-01-15 09:54:06 +0100

    Run simulator via menu

--- modeler/src/main/java/org/informationsystem/ismsuite/modeler/process/commands/PNIDSimulatorCommand.java
+++ modeler/src/main/java/org/informationsystem/ismsuite/modeler/process/commands/PNIDSimulatorCommand.java
@@ -1,24 +1,11 @@
 package org.informationsystem.ismsuite.modeler.process.commands;
 
-import org.eclipse.core.commands.ExecutionEvent;
-import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.ui.IWorkbenchWindow;
-import org.eclipse.ui.handlers.HandlerUtil;
-import org.informationsystem.ismsuite.modeler.commands.AbstractCommand;
+import org.informationsystem.ismsuite.modeler.process.simulator.SimulatorFactory;
 
-public class PNIDSimulatorCommand extends AbstractCommand {
+public class PNIDSimulatorCommand extends ApplicationStartCommand {
 
-	@Override
-	public Object execute(ExecutionEvent event) throws ExecutionException {
-		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
-		
-		MessageDialog.openError(
-				window.getShell(),
-				"PNID Simulator",
-				"Please select a PNID net");
-		return null;
+	public PNIDSimulatorCommand() {
+		super(new SimulatorFactory());
 	}
-
 	
 }
